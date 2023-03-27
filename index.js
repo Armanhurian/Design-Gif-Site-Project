11a296ca5c6fb61c4d91334b166fd204793ab5a4
Arman <hurianarmann@gmail.com>
2023-03-28 03:10:59 +0430

    Add Sign-Up Profile

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ console.log(navbarCompanySubmenu ,navbarResourcesSubmenu , navbarCategoriesSubme
 let $ = document
 
 let navbar = $.querySelector('.navbar')
+let topbarHeaderMenu = $.querySelector('.topbar')
 let navbarCompanyList = $.querySelector('.companyList')
 let navbarResourcesList = $.querySelector('.resourcesList')
 let navbarCategoriesList = $.querySelector('.categoriesList')
@@ -14,6 +15,18 @@ let categoryHeaderContainerListPage1 = $.querySelector('.category-header_contain
 let categoryHeaderContainerListPage2 = $.querySelector('.category-header_container-list-page2')
 let paginationBtn1 = $.querySelector('.paginationBtn1')
 let paginationBtn2 = $.querySelector('.paginationBtn2')
+let navbarRightLinkBtn = $.querySelector('.navbar-right_link-btn')
+let signUpContainer = $.querySelector('.signUp-container')
+let signUpContainerIcon = $.querySelector('.signUp-container-icon')
+let inputGmailSignup = $.querySelector('.inputGmail-signup')
+let inputUsernameSignup = $.querySelector('.inputUsername-signup')
+let inputPasswordSignup = $.querySelector('.inputPassword-signup')
+let signUpContainerBtn = $.querySelector('.signUp-container-btn')
+let gmailParagraph = $.querySelector('.gmail-paragraph')
+let usernameParagraph = $.querySelector('.username-paragraph')
+let passwordParagraph = $.querySelector('.password-paragraph')
+
+
 
 let topbar;
 
@@ -114,4 +127,51 @@ paginationBtn2.addEventListener('click',()=>{
     categoryHeaderContainerListPage2.style.display = 'flex'
     paginationBtn1.classList.remove('btnPaginationDesign')
     paginationBtn2.classList.add('btnPaginationDesign')
+})
+
+navbarRightLinkBtn.addEventListener('click',()=>{
+    topbarHeaderMenu.style.display = 'none'
+    signUpContainer.style.display = 'block'
+    
+    signUpContainerBtn.addEventListener('click',()=>{
+        
+        if(!inputUsernameSignup.value){
+            inputUsernameSignup.style.borderColor = 'red'
+            usernameParagraph.innerHTML = 'Enter Username'
+            usernameParagraph.style.color = 'red'
+            usernameParagraph.style.fontSize = '10px'
+        }else if(!inputPasswordSignup.value){
+            inputPasswordSignup.style.borderColor = 'red'
+            passwordParagraph.innerHTML = 'Enter Password'
+            passwordParagraph.style.color = 'red'
+            passwordParagraph.style.fontSize = '10px'
+            
+        }else if(!inputGmailSignup.value){
+            inputGmailSignup.style.borderColor = 'red'
+            gmailParagraph.innerHTML = 'Enter Gmail'
+            gmailParagraph.style.color = 'red'
+            gmailParagraph.style.fontSize = '10px'
+
+        }else{
+            swal({
+                title:'You have successfully registered',
+                icons:'succsess',
+                buttons:'OK'
+            }).then((result)=>{
+                localStorage.setItem('username',inputUsernameSignup.value)
+                navbarRightLinkBtn.innerHTML = inputUsernameSignup.value
+            })
+        }
+    })
+})
+window.addEventListener('load',()=>{
+    let getLocalUsename = localStorage.getItem('username');
+    navbarRightLinkBtn.innerHTML = getLocalUsename
+
+})
+
+
+signUpContainerIcon.addEventListener('click',()=>{
+    signUpContainer.style.display = 'none'
+    topbarHeaderMenu.style.display = 'block'
 })
